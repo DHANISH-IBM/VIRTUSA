c3c467cb3445741faf01298f337f3b3c23ab1df3
DHANISH-IBM <113742522+DHANISH-IBM@users.noreply.github.com>
2022-11-23 18:17:26 +0530

    Add files via upload

---
+++ Project 2/SpringBoot/src/test/java/net/javaguides/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,31 @@
+package net.javaguides.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+
+
+
+/*package net.javaguides.springboot;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootTest
+class ApplicationIntegrationTest {
+  @Test
+  void contextLoads(ApplicationContext context) {
+    assertThat(context).isNotNull();
+  }
+}*/
